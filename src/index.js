87a08973bc21e82a21e78c63acc77d393c4708e0
Hana <momenthana@kakao.com>
2020-06-27 20:03:06 +0900

    Embed 초기화

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const messages = JSON.parse(fs.readFileSync('src/messages.json').toString())
 
 if (process.env.discordToken) {
   const discord = new Discord.Client()
-  const embed = new Discord.RichEmbed().setColor('#f7cac9')
 
   discord.on('ready', () => {
     console.log(`Logged in as ${discord.user.tag}!`.green)
@@ -16,26 +15,27 @@ if (process.env.discordToken) {
 
   discord.on('message', async msg => {
     try {
+      const embed = new Discord.RichEmbed().setColor('#f7cac9')
+
       if (msg.content.match(/하나.*(핑|ping)|(핑|ping).*하나/)) {
         embed.setTitle(msg.content.includes('핑') ? '퐁!' : 'Pong!')
-        .fields = [
-          { name: 'Discord Server', value: '측정중...' },
-          { name: '지연 시간', value: '측정중...' }
-        ]
+          .fields = [
+            { name: 'Discord Server', value: '측정중...' },
+            { name: '지연 시간', value: '측정중...' }
+          ]
         let ping = await msg.channel.send({ embed })
         embed.fields = [
           { name: 'Discord Server', value: Math.round(discord.ping) + 'ms' },
           { name: '지연 시간', value: ping.createdTimestamp - msg.createdTimestamp + 'ms' }
         ]
         ping.edit({ embed })
-        embed.fields = null
-      }
-      
-      let info = await school(msg.content, msg.channel.id, 'discord', { type: '' })
-      if (info.content) {
-        embed.setTitle(info.title).setDescription(info.content)
-        await msg.channel.send({ embed })
-        console.log(`Discord ${msg.channel.id}\n${msg.content}\n`.green, info.title, info.content)
+      } else {
+        let info = await school(msg.content, msg.channel.id, 'discord', { type: '' })
+        if (info.content) {
+          embed.setTitle(info.title).setDescription(info.content)
+          await msg.channel.send({ embed })
+          console.log(`Discord ${msg.channel.id}\n${msg.content}\n`.green, info.title, info.content)
+        }
       }
     } catch (error) {
       console.warn(`Discord ${msg.channel.id}\n${msg.content}\n`.red, error)
