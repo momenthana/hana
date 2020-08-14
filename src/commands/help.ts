7c56f29308d60213e6328fcad024ebab461b798d
Hana <momenthana@kakao.com>
2020-08-14 16:31:43 +0900

    도움말 Command list로 수정

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -3,9 +3,12 @@ import { load } from '../utils'
 const messages = load('src/messages.json')
 
 const help = async (text, embed) => {
-  if (text.match(/(도움|도와|help)/)) {
-    embed.setTitle('하나')
-      .setDescription(messages.help)
+  if (text.match(/(도움|도와줘|help)/)) {
+    embed.setTitle('도움말')
+      .addField('하나야 급식, 내일 급식, 월요일 급식, 3일 뒤 급식', '급식 정보는 다양하게 확인할 수 있어')
+      .addField('하나야 일정', '일정 정보를 확인할 수 있어')
+      .addField('하나야 [학교명] 검색', '채널마다 학교를 등록할 수 있어')
+      .addField('하나야 도와줘', '도움말 확인이 가능해')
   }
 }
 
