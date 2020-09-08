c81abcfbdf2c386e523014bd949867eb1348335f
Hana <momenthana@kakao.com>
2020-09-08 19:52:21 +0900

    Uptime 단위 추가

--- src/commands/uptime.ts
+++ src/commands/uptime.ts
@@ -1,6 +1,12 @@
-const uptime = async (text, embed) => {
+const uptime = (text, embed) => {
+  let uptime = process.uptime()
+  let sec = Math.floor(uptime % 60)
+  let min = Math.floor(uptime / 60 % 60)
+  let hour = Math.floor(uptime / 60 / 60 % 24)
+  let day = Math.floor(uptime / 60 / 60 / 24)
+
   embed.setTitle('Uptime!')
-    .setDescription(Math.floor(process.uptime()) + '초')
+    .setDescription(`${day ? day + '일' : ''} ${hour ? hour + '시간' : ''} ${min ? min + '분' : ''} ${sec}초`)
 }
 
 export default uptime
