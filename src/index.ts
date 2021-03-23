59fefb73d01bd2c1b9fbf4596c6b48d8fa8fd9ac
Hana <momenthana@kakao.com>
2021-03-23 16:33:26 +0900

    feat sharding manager

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,9 @@
-import Discord from 'discord.js'
-import colors from 'colors'
-import fs from 'fs'
-import { help, invite, meal, ping, remove, schedule, search, uptime } from './commands'
-import { embed } from './utils'
+import { ShardingManager } from "discord.js"
 
-const commands = {
-  'help|도움|도와줘|명령': help,
-  'invite|초대링크|초대주소': invite,
-  'meal|아침|조식|점심|중식|저녁|석식|급식|식단|학식': meal,
-  'ping|핑': ping,
-  'remove|삭제|제거': remove,
-  'schedule|일정': schedule,
-  'search|검색|등록': search,
-  'uptime|업타임': uptime,
-}
+const manager = new ShardingManager("./src/bot.ts", {
+  token: process.env.discordToken,
+  execArgv: ["-r", "ts-node/register"],
+})
 
-const messages = JSON.parse(fs.readFileSync('src/messages.json').toString())
-
-if (process.env.discordToken) {
-  const discord = new Discord.Client()
-
-  discord.on('ready', () => {
-    console.log(colors.green(`Logged in as ${discord.user.tag}!`))
-  })
-
-  discord.on('message', async msg => {
-    try {
-      if (msg.author.bot) return
-
-      if (msg.system) {
-        if (msg.type == 'GUILD_MEMBER_JOIN') msg.react('👋')
-        else if (msg.type == 'PINS_ADD') msg.react('📌')
-        return
-      }
-
-      if (process.env.test) console.log(colors.green(msg.channel.id), colors.yellow(msg.author.username), msg.content)
-
-      if (!(msg.content.includes(discord.user.username)) != (msg.mentions.users.first() ? discord.user.id == msg.mentions.users.first().id : false)) return
-
-      for (const [regexp, command] of Object.entries(commands)) {
-        if (msg.content.match(RegExp(regexp, 'i'))) {
-          await command({msg, discord, embed: embed(msg)})
-        }
-      }
-    } catch (error) {
-      console.warn(error)
-    }
-  })
-
-  setInterval(() => {
-    discord.user.setActivity(eval(messages.activity[Math.floor(Math.random() * messages.activity.length)]), {
-      type: process.env.twitch ? 'STREAMING' : null,
-      url: 'https://www.twitch.tv/' + process.env.twitch
-    })
-  }, 10000)
-
-  discord.login(process.env.discordToken)
-}
+manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`))
+manager.spawn()
